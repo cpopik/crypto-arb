63583a4b3dcda0ca3bf650fe4cab54607c4dfd66
Connor Popik <popik@mit.edu>
2017-07-19 13:18:41 -0400

    added example secrets file

--- coinbase.py
+++ coinbase.py
@@ -1,1 +1,15 @@
 # api wrapper
+
+import coinbase
+
+from secrets import *
+
+
+def main():
+    print(COINBASE_PASS)
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ secrets_EXAMPLE.py
@@ -0,0 +1,6 @@
+# secrets.py
+
+# fill out so not under version control
+
+COINBASE_USER = 'blah'
+COINBASE_PASS = 'blah'
